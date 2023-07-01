2a3b72e9c19a62566bcfd27badf9326de8dc3d26
chris <chris.cb.culver@gmail.com>
2023-07-01 09:53:51 +0100

    Working on pion

--- examples/pion.py
+++ examples/pion.py
@@ -1,8 +1,14 @@
 from PyCorrelationMatrixManager.correlation_matrix import CorrelationMatrix
 from make_ops import *
 
-create=create_pion_op()
-annihilate=annihilate_pion_op
+create=[create_pion_op()]
+annihilate=[annihilate_pion_op()]
+
+gammas=[]
+
+print(create[0])
+print(annihilate[0])
 
 
-cmat = CorrelationMatrix(create, annihilate)
+cmat = CorrelationMatrix(create, annihilate, gammas, [0], [0], 0)
+cmat.run()
